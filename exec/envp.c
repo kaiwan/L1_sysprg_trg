127ab21d29d2b1f022db011f22b8aac485d19aa2
kaiwan nb <kaiwan.billimoria@gmail.com>
2018-04-08 08:24:46 +0530

    exec: initial commit

---
+++ exec/envp.c
@@ -0,0 +1,57 @@
+/* envp.c:
+ *
+ * Demonstrate explicit environment passing with execle() .
+ * Execute the printenv command after changing the environment
+ * value of the TERM variable to dialup.
+ *
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXENV	4096
+
+int main(argc, argv, arge)
+int argc;
+char *argv[];
+char *arge[];
+{
+	int i, j;
+	static char *envp[MAXENV];	/* Pointers to environment */
+	char *s = "TERM=dialup";
+
+	for (i = 0; arge[i] != NULL; i++) {
+		envp[i] = arge[i];
+	}			/* for */
+	envp[i] = NULL;
+
+	/* Search for TERM variable in environment.
+	 * Note the use of the strncpy (as opposed to strcpy) : this is for 
+	 * security, helping prevent buffer overflow attacks.
+	 */
+	j = 0;
+	while ((strncmp("TERM=", envp[j], 5) != 0) && (j < (i - 1)))
+		j++;
+	if (j >= (i - 1)) {
+		fprintf(stderr, "%s: env variable TERM not found. Aborting..\n",
+			argv[0]);
+		exit(1);
+	}
+	envp[j] = s;
+
+	/*
+	   gcc gives: envp.c:42: warning: missing sentinel in function call
+	   Solution:
+	   From the man page:
+	   "The list of arguments must be terminated by a NULL pointer, and, 
+	   since these are variadic functions, this pointer must be 
+	   cast (char *) NULL"
+	 */
+	if (execle("/usr/bin/printenv", "printenv", (char *)0, envp) < 0) {
+		perror(argv[0]);
+		exit(1);
+	}			/* if */
+	exit(0);		// just to get rid of the compiler warning 
+	// ("warning: control reaches end of non-void function")
+}				/* main */
---
+++ exec/exec_poc.c
@@ -0,0 +1,34 @@
+/*
+ * exec_poc.c
+ * 
+ * Trivial demo of using execl() to execute a process.
+ * 
+ * Shows that the PID of the predecessor and successor are the same.
+ *
+ * Author: Kaiwan N Billimoria <kaiwan@kaiwantech.com>
+ * License: GNU [L]GPL v2
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	printf("Predecessor \"%s\": PID is %d\n", argv[0], getpid());
+	printf("%s: will exec \"ps\" now..\n", argv[0]);
+	/*
+	   gcc gives: envp.c:42: warning: missing sentinel in function call
+	   Solution:
+	   From the man page:
+	   "The list of arguments must be terminated by a NULL pointer, and, 
+	   since these are variadic functions, this pointer must be 
+	   cast (char *) NULL"
+	 */
+	if (execl("/bin/ps", "ps", (char *)0) == -1) {
+		perror("execl failed");
+		exit(1);
+	}
+	// this line will never be reached on execl() success
+	exit(0);		// just to get rid of compiler warning
+}
