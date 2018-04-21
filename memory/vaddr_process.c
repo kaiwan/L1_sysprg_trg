f77f173d356408952b0998d1206a42cd80503ff8
kaiwan nb <kaiwan.billimoria@gmail.com>
2018-04-21 19:07:57 +0530

    Initial commit - memory

---
+++ memory/vaddr_process.c
@@ -0,0 +1,43 @@
+/* vaddr_process.c
+ *
+ * Display the virtual addresses of different regions of the process VAS.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//#define 32BIT
+#undef BITS_32
+
+#ifdef BITS_32
+	#define FMTSPC "%08x"
+	#define TYPECST unsigned int
+#else /* 64-bit */
+	#define FMTSPC "%016lx"
+	#define TYPECST unsigned long
+#endif
+typedef unsigned int u32;
+
+extern char *environ;
+int gi=5, gu;
+
+int main(int argc, char **argv)
+{
+	char local=0xff, *heapmem;
+	int i=1, j=2, k=3;
+
+	heapmem = malloc(1024);
+	printf ("PID %d: virtual addresses:"
+			"\n&main    = 0x" FMTSPC \
+			"\n&gi      = 0x" FMTSPC \
+			"\n&gu      = 0x" FMTSPC \
+			"\n&heapmem = 0x" FMTSPC \
+			"\n&local   = 0x" FMTSPC \
+			"\n&environ = 0x" FMTSPC "\n",
+		getpid(), (TYPECST) &main, (TYPECST) &gi, (TYPECST) &gu, 
+		(TYPECST) heapmem, (TYPECST) &local, (TYPECST)environ);
+	printf("\n... In pause now... ^C to end\n");
+	pause();
+	free (heapmem);
+	exit (0);
+}
