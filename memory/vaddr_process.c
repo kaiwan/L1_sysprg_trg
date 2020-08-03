4b5c22969db1c363012686cfa94032e33dbd3aa7
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-08-03 17:41:16 +0530

    Using the 'better' Makefile; +cleanup

--- memory/vaddr_process.c
+++ memory/vaddr_process.c
@@ -6,38 +6,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-//#define 32BIT
 #undef BITS_32
 
 #ifdef BITS_32
-	#define FMTSPC "%08x"
-	#define TYPECST unsigned int
-#else /* 64-bit */
-	#define FMTSPC "%016lx"
-	#define TYPECST unsigned long
+#define FMTSPC "%08x"
+#define TYPECST unsigned int
+#else				/* 64-bit */
+#define FMTSPC "%016lx"
+#define TYPECST unsigned long
 #endif
 typedef unsigned int u32;
 
 extern char *environ;
-int gi=5, gu;
+int gi = 5, gu;
 
 int main(int argc, char **argv)
 {
-	char local=0xff, *heapmem;
-	int i=1, j=2, k=3;
+	char local = 0xff, *heapmem;
+	int i = 1, j = 2, k = 3;
 
 	heapmem = malloc(1024);
-	printf ("PID %d: virtual addresses:"
-			"\n&main    = 0x" FMTSPC \
-			"\n&gi      = 0x" FMTSPC \
-			"\n&gu      = 0x" FMTSPC \
-			"\n&heapmem = 0x" FMTSPC \
-			"\n&local   = 0x" FMTSPC \
-			"\n&environ = 0x" FMTSPC "\n",
-		getpid(), (TYPECST) &main, (TYPECST) &gi, (TYPECST) &gu, 
-		(TYPECST) heapmem, (TYPECST) &local, (TYPECST)environ);
+	printf("PID %d: virtual addresses:"
+	       "\n&main    = 0x" FMTSPC
+	       "\n&gi      = 0x" FMTSPC
+	       "\n&gu      = 0x" FMTSPC
+	       "\n&heapmem = 0x" FMTSPC
+	       "\n&local   = 0x" FMTSPC
+	       "\n&environ = 0x" FMTSPC "\n",
+	       getpid(), (TYPECST) & main, (TYPECST) & gi, (TYPECST) & gu,
+	       (TYPECST) heapmem, (TYPECST) & local, (TYPECST) environ);
 	printf("\n... In pause now... ^C to end (can run vasu_grapher !)\n");
 	pause();
-	free (heapmem);
-	exit (0);
+	free(heapmem);
+	exit(0);
 }
