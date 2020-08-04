5ddd6785af421a42086704e467690a08c7640bb3
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-08-04 06:49:27 +0530

    reorgaized: using the 'better' Makefile; +cleanup

---
+++ sched/sched_pthrd_rtprio/runit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# runit.sh
+
+[ ! -f sched_pthrd_rtprio_dbg ] && {
+  echo "$0: build it first pl..."
+  exit 1
+}
+# Allow it to only execute on core #1
+#sudo taskset -c 01 ./sched_pthrd_rtprio_dbg 20
+
+# There's a better approach to using sudo; we use the powerful POSIX
+# Capabilities model instead! This way, the process (and threads) get _only_
+# the capabilities they require and nothing more. Reduces the attack surface
+echo "[+] sudo setcap CAP_SYS_NICE+eip ./sched_pthrd_rtprio_dbg"
+sudo setcap CAP_SYS_NICE+eip ./sched_pthrd_rtprio_dbg
+echo "[+] getcap ./sched_pthrd_rtprio_dbg"
+getcap ./sched_pthrd_rtprio_dbg
+
+# Still have to run it on exactly one core though!!
+echo "[+] taskset -c 01 ./sched_pthrd_rtprio_dbg 20"
+taskset -c 01 ./sched_pthrd_rtprio_dbg 20
