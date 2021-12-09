6ad800a6e7f91fbcd99f9e06574bd4734228b35d
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-12-10 01:45:28 +0530

    buggy ver of manythrds prg

---
+++ pthreads/misc/cr8ManyThrds/buggy_manythrds/cr8ManyThrds.c
@@ -0,0 +1,55 @@
+/*
+ * cr8ManyThrds.c
+ * (c) Kaiwan NB, kaiwanTECH
+ * License: MIT
+ */
+#define _POSIX_C_SOURCE    200112L	/* or earlier: 199506L */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX_THREADS    127000 // 50000	// arbitrary
+typedef unsigned int u32;
+typedef unsigned long u64;
+
+char gbuf[100];
+
+void *PrintStuff(void *threadnum)
+{
+	printf("Thread %05ld\n", (long)threadnum);
+	pause();
+	pthread_exit(NULL);
+}
+
+int main(int argc, char **argv)
+{
+	pthread_t threads[5];
+	int numthrds;
+	long t;
+
+	if (argc != 2) {
+		printf("Usage: %s num-threads-to-create\n", argv[0]);
+		exit(1);
+	}
+	numthrds = atoi(argv[1]);
+	if ((numthrds <= 0) || (numthrds > MAX_THREADS)) {
+		printf("%s: num-threads invalid\n", argv[0]);
+		exit(1);
+	}
+
+	for (t = 0; t < numthrds; t++) {
+		int rc;
+		rc = pthread_create(&threads[t], NULL, PrintStuff, (void *)t);
+		if (rc) {
+			printf
+			    ("%s: Thrd # %ld: ERROR: return code from pthread_create() is %d\n",
+			     argv[0], t, rc);
+			perror("pthread_create failed");
+			exit(1);
+		}
+	}
+	pause();
+	pthread_exit(NULL);
+}
