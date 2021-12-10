d724e96e5992bb4697a64f55df8cdb13222357ba
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-12-10 09:31:32 +0530

    cleanup

--- pthreads/llnl-tut-prgs/pt2-join.c
+++ pthreads/llnl-tut-prgs/pt2-join.c
@@ -7,17 +7,17 @@
 #include <unistd.h>
 
 #define NUM_THREADS	2
-//static int NUM_THREADS=2;
 
-void *BusyWork(void *null)
+void *BusyWork(void *n)
 {
 	int i;
 	double result = 0.0;
+
 	for (i = 0; i < 1000000; i++) {
 		result = result + 1;
 		//result = result + (double)random();
 	}
-	printf("result = %f\n", result);
+	printf("worker %ld: result = %f\n", (long)n, result);
 	pthread_exit((void *)0);
 }
 
@@ -25,7 +25,8 @@ int main()
 {
 	pthread_t work_thrd[NUM_THREADS];
 	pthread_attr_t attr;
-	int rc, t, status;
+	int rc, status;
+	long t;
 
 	/* Initialize and set thread detached attribute */
 	pthread_attr_init(&attr);
@@ -33,13 +34,11 @@ int main()
 
 	printf("NUM_THREADS = %d\n", NUM_THREADS);
 	for (t = 0; t < NUM_THREADS; t++) {
-		printf("Creating thread %d\n", t);
-		rc = pthread_create(&work_thrd[t], &attr, BusyWork, NULL);
+		printf("Creating thread %ld\n", t);
+		rc = pthread_create(&work_thrd[t], &attr, BusyWork, (void *)t);
 		if (rc) {
-			printf
-			    ("ERROR; return code from pthread_create() is %d\n",
-			     rc);
-			exit(-1);
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(EXIT_FAILURE);
 		}
 	}
 
@@ -47,15 +46,14 @@ int main()
 	pthread_attr_destroy(&attr);
 	printf("NUM_THREADS = %d\n", NUM_THREADS);
 	for (t = 0; t < NUM_THREADS; t++) {
-			printf("join loop # %d\n", t);
+		printf("join loop # %ld\n", t);
 		/* Note: "status" should not be local to the dying thread */
 		rc = pthread_join(work_thrd[t], (void **)&status);
 		if (rc) {
-			printf("ERROR; return code from pthread_join() is %d\n",
-			       rc);
-			exit(-1);
+			printf("ERROR; return code from pthread_join() is %d\n", rc);
+			exit(EXIT_FAILURE);
 		}
-		printf("main: completed join with thread %d status %d\n", t, status);
+		printf("main: completed join with thread %ld status %d\n", t, status);
 		sleep(1);
 	}
 
