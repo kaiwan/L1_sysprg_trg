741c7992473c6839d88ee1aea42135d0c8aac87b
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-09-01 18:16:59 +0530

    Cleanups..

--- ipc/posix_ipc/mq_posix/recv_mq.c
+++ ipc/posix_ipc/mq_posix/recv_mq.c
@@ -5,18 +5,18 @@
  This program is the "consumer" (or receiver) app.
 
  Should mount the 'mqueue' filesystem as root:
- # mkdir /dev/mq
- # mount -t mqueue none /dev/mq
+ # mkdir /dev/mqueue
+ # mount -t mqueue none /dev/mqueue
 
  Can then see that messages have been written by the sender...
  $ mount |grep mqueue
- none on /dev/mq type mqueue (rw)
+ none on /dev/mqueue type mqueue (rw)
  $ 
  
- $ l /dev/mq/
+ $ l /dev/mqueue/
  total 0
  -rw-r--r-- 1 kaiwan kaiwan 80 May 28 12:15 myposixmq
- $ cat /dev/mq/myposixmq 
+ $ cat /dev/mqueue/myposixmq 
  QSIZE:30         NOTIFY:0     SIGNO:0     NOTIFY_PID:0
  $ 
 
@@ -41,56 +41,39 @@
 int main(int argc, char **argv)
 {
 	mqd_t mymq;
+	struct mq_attr attr;
 
-#if 0
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s \"msg to send\" priority\n", argv[0]);
-		exit (1);
-	}
-
-	int len = strlen(argv[1]);
-	if (len <= 0) {
-		fprintf(stderr, "%s: invalid message? Aborting...\n", argv[0]);
-		exit (1);
-	}
-
-	unsigned prio=atoi(argv[2]);
-	if ((prio < 0) || (prio > sysconf(_SC_MQ_PRIO_MAX))) {
-		fprintf(stderr, "%s: invalid priority value, aborting...\n", argv[0]);
-		exit (1);
-	}
-	//printf("max prio: %ld\n", sysconf(_SC_MQ_PRIO_MAX));
-#endif
-
-	mymq = mq_open(MQNAME, O_RDWR|O_CREAT|O_EXCL, 0644, NULL); // 'attr' as NULL => use defaults
-	if (-1 == mymq) {
-		if (EEXIST == errno) { // failed as the MQ already exists!
+	mymq = mq_open(MQNAME, O_RDWR | O_CREAT | O_EXCL, 0644, NULL);	// 'attr' as NULL => use defaults
+	if (mymq == -1) {
+		if (EEXIST == errno) {	// failed as the MQ already exists!
 			mymq = mq_open(MQNAME, O_RDWR);
-			if (-1 == mymq)
+			if (mymq == -1)
 				handle_error("mq_open failed");
 		}
 	}
 
-	while (1) {
-		struct mq_attr attr;
-		char *buf=NULL;
-		ssize_t nr=0;
-		unsigned prio=0;
-
-		/* Determine max msg size; allocate buffer to receive msg */
-		if (mq_getattr (mymq, &attr) == -1)
-			handle_error("mq_getattr failed");
-		buf = calloc(attr.mq_msgsize, 1);
-		if (!buf)
-			handle_error("calloc failed");
-
-		if ((nr = mq_receive (mymq, buf, attr.mq_msgsize, &prio)) == -1)
-			handle_error("mq_receive failed");
-		printf("%s: Received:\n \"%s\" [%d bytes, prio %u]\n", argv[0], buf, nr, prio);
-		free (buf);
+	/* Determine max msg size (typically 8192 bytes); allocate buffer to receive msg */
+	if (mq_getattr(mymq, &attr) == -1)
+		handle_error("mq_getattr failed");
+	char *buf = calloc(attr.mq_msgsize, 1);
+	if (!buf) {
+		mq_close(mymq);
+		handle_error("calloc failed");
 	}
 
-	mq_close(mymq);
-	exit (0);
-}
+	while (1) {
+		ssize_t nr = 0;
+		unsigned prio = 0;
 
+		if ((nr = mq_receive(mymq, buf, attr.mq_msgsize, &prio)) == -1) {
+			mq_close(mymq);
+			handle_error("mq_receive failed");
+		}
+		printf("%s: Received:\n \"%s\" [%ld bytes, prio %u]\n", argv[0],
+		       buf, nr, prio);
+	}
+
+	free(buf);
+	mq_close(mymq);
+	exit(0);
+}
--- ipc/posix_ipc/mq_posix/sender_mq.c
+++ ipc/posix_ipc/mq_posix/sender_mq.c
@@ -6,17 +6,17 @@
 
  Should mount the 'mqueue' filesystem as root:
  # mkdir /dev/mq
- # mount -t mqueue none /dev/mq
+ # mount -t mqueue none /dev/mqueue
 
  Can then see that messages have been written...
  $ mount |grep mqueue
- none on /dev/mq type mqueue (rw)
+ none on /dev/mqueue type mqueue (rw)
  $ 
  
- $ l /dev/mq/
+ $ l /dev/mqueue/
  total 0
  -rw-r--r-- 1 kaiwan kaiwan 80 May 28 12:15 myposixmq
- $ cat /dev/mq/myposixmq 
+ $ cat /dev/mqueue/myposixmq 
  QSIZE:30         NOTIFY:0     SIGNO:0     NOTIFY_PID:0
  $ 
 
@@ -24,8 +24,19 @@
   There's definitely a race here between sender and receiver; fix it 
   using the POSIX semaphore!
 
-  (c) 2012, kaiwan billimoria, [L]GPL.
- */
+(c) 2012, kaiwan billimoria.
+License: MIT
+
+***
+Notice that, in the mq_send() API, the parameter to the message is a ptr to a string:
+int mq_send(mqd_t mqdes, const char *msg_ptr,
+                     size_t msg_len, unsigned int msg_prio);
+So can one send stuff besides ASCII? Yes, via a structure too.
+FYI, a sample prg showing how to do so can be found here:
+https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/MQueues.html
+(Code listing 3.9)
+***
+*/
 #include <pthread.h>
 #include <mqueue.h>
 #include <stdio.h>
@@ -43,36 +54,39 @@ int main(int argc, char **argv)
 	mqd_t mymq;
 
 	if (argc != 3) {
-		fprintf(stderr, "Usage: %s \"msg to send\" priority\n", argv[0]);
-		exit (1);
+		fprintf(stderr, "Usage: %s \"msg to send\" priority\n",
+			argv[0]);
+		exit(1);
 	}
 
 	int len = strlen(argv[1]);
 	if (len <= 0) {
 		fprintf(stderr, "%s: invalid message? Aborting...\n", argv[0]);
-		exit (1);
+		exit(1);
 	}
 
-	unsigned prio=atoi(argv[2]);
+	unsigned prio = atoi(argv[2]);
 	if ((prio < 0) || (prio > sysconf(_SC_MQ_PRIO_MAX))) {
-		fprintf(stderr, "%s: invalid priority value, aborting...\n", argv[0]);
-		exit (1);
+		fprintf(stderr, "%s: invalid priority value, aborting...\n",
+			argv[0]);
+		exit(1);
 	}
-	//printf("max prio: %ld\n", sysconf(_SC_MQ_PRIO_MAX));
+	printf("prio: %d, max prio: %ld\n", prio, sysconf(_SC_MQ_PRIO_MAX));
 
-	mymq = mq_open(MQNAME, O_RDWR|O_CREAT|O_EXCL, 0644, NULL); // 'attr' as NULL => use defaults
-	if (-1 == mymq) {
-		if (EEXIST == errno) { // failed as the MQ already exists!
+	mymq = mq_open(MQNAME, O_RDWR | O_CREAT | O_EXCL, 0644, NULL);	// 'attr' as NULL => use defaults
+	if (mymq == -1) {
+		if (EEXIST == errno) {	// failed as the MQ already exists!
 			mymq = mq_open(MQNAME, O_RDWR);
-			if (-1 == mymq)
+			if (mymq == -1)
 				handle_error("mq_open failed");
 		}
 	}
 
-	if (mq_send (mymq, argv[1], len, prio) == -1)
+	if (mq_send(mymq, argv[1], len, prio) == -1) {
+		mq_close(mymq);
 		handle_error("mq_send failed");
+	}
 
 	mq_close(mymq);
-	exit (0);
+	exit(0);
 }
-
