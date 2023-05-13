9f05bb3a280c320f93f69b81cb28b3420e30c439
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-05-13 13:18:42 +0530

    rename var r to pi

--- assert_try/assert_try2.c
+++ assert_try/assert_try2.c
@@ -5,19 +5,19 @@
 static void foo(void)
 {
 #if 1
-	double r, q; // hey, locals are uninitialized! 
+	double pi, q; // hey, locals are uninitialized! 
 #else
-	double r = 3.14, q;
+	double pi = 3.14, q;
 #endif
 	int factorx = 3;
 
 #if 1
-	// expect / assume r to be 3.14
-	assert(r == 3.14);
+	// expect / assume pi to be 3.14
+	assert(pi == 3.14);
 #endif
-	q = r*factorx; // possible UMR bug ! the assertion will prevent it
-	// from occuring...
-	printf("r=%.2f, q=%.2f\n", r, q);
+	q = pi*factorx; /* possible UMR bug ! the assertion will prevent it
+						from occuring... */
+	printf("pi=%.2f, q=%.2f\n", pi, q);
 }
 
 int main()
