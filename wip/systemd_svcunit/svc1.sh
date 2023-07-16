6a55fd786a5b151b9b43aa5763de1f33ace8927b
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-07-16 19:42:24 +0530

    Update svc units, script to use the OSBoxes VM paths..

--- wip/systemd_svcunit/svc1.sh
+++ wip/systemd_svcunit/svc1.sh
@@ -4,5 +4,7 @@
 
 # If all's running well, the echo (stdout/stderr) automatically goes to the syslog !
 echo "$0: am running!"
-/home/kaiwan/gitL1/wip/systemd_svcunit/primegen/primegen 100000
+# UPDATE path/to/executable !
+#/home/kaiwan/gitL1/wip/systemd_svcunit/primegen/primegen 100000
+/home/osboxes/kaiwanTECH/L1_sysprg_trg/wip/systemd_svcunit/primegen/primegen 100000
 exit 0
