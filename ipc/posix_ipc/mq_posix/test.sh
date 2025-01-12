1a1a83931dd8eeb11759cf1a28dfd1d00a9020a3
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2025-01-12 11:02:16 +0530

    test.sh: check for & issue make if reqd

--- ipc/posix_ipc/mq_posix/test.sh
+++ ipc/posix_ipc/mq_posix/test.sh
@@ -3,6 +3,8 @@
 SENDER=./sender_mq
 RECEIVER=./recv_mq
 
+[[ ! -f ${SENDER} || ! -f ${RECEIVER} ]] && make
+
 mount |grep mqueue > /dev/null 2>&1 || {
   mkdir /dev/mq 2>/dev/null
   mount -t mqueue none /dev/mq
