d3d3a6073a70a9901a0b476efbb80ae7fad0a603
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2026-04-11 10:34:00 +0530

    Improve demo output (and comments); + rm sem each time

--- ipc/posix_ipc/more_demos/appdemo_shm_sem_as_mtx/appdemo_shm_sem_as_mtx.c
+++ ipc/posix_ipc/more_demos/appdemo_shm_sem_as_mtx/appdemo_shm_sem_as_mtx.c
@@ -8,35 +8,64 @@
 Demo runs:
 
 1. Correct way:
-$ ./appdemo_shmsem 1
-56930: writing char p for 10485760 bytes at offset 0 now
-56931: writing char c for 10485760 bytes at offset 1048576 now
-appdemo_shmsem $ hexdump --canonical /dev/shm/myshm_mtx_demo 
+|appdemo_shm_sem_as_mtx $ ./appdemo_shm_sem_as_mtx 1
+(Mutex/binary) Semaphore setup and init to value 0
+1607336: writing char p for chunk 0 at offset 0 now
+Child is waiting to acquire the mutex (sem) now...
+1607336: writing char p for chunk 1 at offset 4096 now
+1607336: writing char p for chunk 2 at offset 8192 now
+1607336: writing char p for chunk 3 at offset 12288 now
+1607336: writing char p for chunk 4 at offset 16384 now
+1607336: writing char p for chunk 5 at offset 20480 now
+1607337: writing char c for chunk 0 at offset 2560 now
+1607337: writing char c for chunk 1 at offset 6656 now
+1607337: writing char c for chunk 2 at offset 10752 now
+1607337: writing char c for chunk 3 at offset 14848 now
+1607337: writing char c for chunk 4 at offset 18944 now
+1607337: writing char c for chunk 5 at offset 23040 now
+|appdemo_shm_sem_as_mtx $ hexdump --canonical /dev/shm/myshm_mtx_demo 
 00000000  70 70 70 70 70 70 70 70  70 70 70 70 70 70 70 70  |pppppppppppppppp|
 *
-00100000  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
+00000a00  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
 *
-00b00000  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+00006e00  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
 *
-01400000
-$ 
+0xa00 = 2560
 
-2. WRONG way (no sem):
-$ ./appdemo_shmsem 0
-56988: writing char p for 10485760 bytes at offset 0 now
-56989: writing char c for 10485760 bytes at offset 1048576 now
-appdemo_shmsem $ hexdump --canonical /dev/shm/myshm_mtx_demo 
+2. WRONG way:
+
+|appdemo_shm_sem_as_mtx $ ./appdemo_shm_sem_as_mtx 0
+(Mutex/binary) Semaphore setup and init to value 0
+1607342: writing char p for chunk 0 at offset 0 now
+Child is NOT using the mutex (sem) now...
+1607343: writing char c for chunk 0 at offset 2560 now
+1607342: writing char p for chunk 1 at offset 4096 now
+1607343: writing char c for chunk 1 at offset 6656 now
+1607342: writing char p for chunk 2 at offset 8192 now
+1607343: writing char c for chunk 2 at offset 10752 now
+1607342: writing char p for chunk 3 at offset 12288 now
+1607343: writing char c for chunk 3 at offset 14848 now
+1607342: writing char p for chunk 4 at offset 16384 now
+1607343: writing char c for chunk 4 at offset 18944 now
+1607342: writing char p for chunk 5 at offset 20480 now
+1607343: writing char c for chunk 5 at offset 23040 now
+|appdemo_shm_sem_as_mtx $ 
+|appdemo_shm_sem_as_mtx $ hexdump --canonical /dev/shm/myshm_mtx_demo 
 00000000  70 70 70 70 70 70 70 70  70 70 70 70 70 70 70 70  |pppppppppppppppp|
 *
-0036f000  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
+00000a00  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
 *
-003d3000  70 70 70 70 70 70 70 70  70 70 70 70 70 70 70 70  |pppppppppppppppp|
+00001000  70 70 70 70 70 70 70 70  70 70 70 70 70 70 70 70  |pppppppppppppppp|
 *
-00434000  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
+00001a00  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
 *
-00457000  70 70 70 70 70 70 70 70  70 70 70 70 70 70 70 70  |pppppppppppppppp|
+00002000  70 70 70 70 70 70 70 70  70 70 70 70 70 70 70 70  |pppppppppppppppp|
 *
-00475000  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
+00002a00  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
+*
+00003000  70 70 70 70 70 70 70 70  70 70 70 70 70 70 70 70  |pppppppppppppppp|
+*
+00003a00  63 63 63 63 63 63 63 63  63 63 63 63 63 63 63 63  |cccccccccccccccc|
 *
 [ ... ]
 
@@ -59,38 +88,45 @@ appdemo_shmsem $ hexdump --canonical /dev/shm/myshm_mtx_demo
 #define SHM_NAME "/myshm_mtx_demo"
 #define SHM_MODE 0640
 #define MEMSIZE 1024*1024*20
-#define LEN 1024*1024
+#define LEN 2560
 
 #define SEM_NAME "/appdemo_mtx"	// under /dev/shm
 static sem_t *sem;
 
-static void sem_setup(void)
+static inline void sem_setup(void)
 {
+	sem_unlink(SEM_NAME);  // clean up stale semaphore, ignore errors
 	/* Create and open the semaphore */
 	sem = sem_open(SEM_NAME, O_CREAT | O_EXCL, 0600, 0);
 	if (sem == SEM_FAILED) {
-		if (errno == EEXIST) {
-			sem = sem_open(SEM_NAME, 0);
-			if (sem == SEM_FAILED) {
-				perror("sem_open() failed");
-				exit(1);
-			}
-		}
+		perror("sem_open() failed");
+		exit(1);
 	}
-	printf("(Mutex) Semaphore setup and init to value 0\n");
+	printf("(Mutex/binary) Semaphore setup and init to value 0\n");
 }
 
-/* Write len bytes of char 'letter' to region p_udata starting at offset 'offset' */
+/* Write len bytes of char 'letter' to region p_udata starting at offset 'offset'
+ * Splits the write into page-sized chunks
+ */
 static void update_udata(void *p_udata, char letter, off_t offset, size_t len)
 {
-	printf("%d: writing char %c for %zu bytes at offset %zu now\n",
-	       getpid(), letter, len, offset);
-	memset(p_udata + offset, letter, len);
+	const size_t chunk_sz = 4096;	/* one page per iteration */
+	size_t num_iters = len / chunk_sz;
+	size_t remainder = len % chunk_sz;
+
 	/* Note: from cppcheck:
 	portability: 'p_udata' is of type 'void *'. When using void pointers in calculations,
 	the behaviour is undefined. Arithmetic operations on 'void *' is a GNU C extension,
-	which defines the 'sizeof(void)' to be 1. [arithOperationsOnVoidPointer]
-	 */	   
+	which defines the 'sizeof(void)' to be 0. [arithOperationsOnVoidPointer]
+	 */
+	for (size_t i = 0; i < num_iters; i++) {
+		printf("%d: writing char %c for chunk %zu at offset %zu now\n",
+		       getpid(), letter, i, offset + (i * chunk_sz));
+		memset(p_udata + offset + (i * chunk_sz), letter, chunk_sz);
+		usleep(1000);	/* 1ms */
+	}
+	if (remainder)
+		memset(p_udata + offset + (num_iters * chunk_sz), letter, remainder);
 }
 
 int main(int argc, char **argv)
@@ -154,9 +190,12 @@ int main(int argc, char **argv)
 			 * - the default - it's forced to wait on it being
 			 * incremeneted to 1 by the parent calling sem_post()
 			 */
+			printf("Child is waiting to acquire the mutex (sem) now...\n");
 			if (sem_wait(sem) == -1)
 				perror("sem_wait() failed");
 		}
+		else
+			printf("Child is NOT using the mutex (sem) now...\n");
 		//static void update_udata(void *p_udata, char letter, off_t offset, size_t len)
 		update_udata(p_udata, 'c', LEN, LEN * 10);
 		munmap(p_udata, MEMSIZE);
@@ -183,9 +222,10 @@ int main(int argc, char **argv)
 		wait(0);
 	}
 
-	/* Unmap, close, and delete the shared memory object */
+	// Clean up
 	munmap(p_udata, MEMSIZE);
 	close(shmfd);
 	if (use_mtx)
 		sem_close(sem);
+	sem_unlink(SEM_NAME);
 }
